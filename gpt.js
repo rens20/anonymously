9f6746074400fa9c9b2c43e203cb76a4c723b812
wasieDev <127023454+rens20@users.noreply.github.com>
2023-10-07 10:51:50 +0800

    Update gpt.js

--- gpt.js
+++ gpt.js
@@ -1,4 +1,116 @@
+// Initialize an empty posts array (in-memory "database")
 let posts = [];
+ 
+async function shoti() {
+    try {
+        const response = await fetch('https://liby.wasieacuna.repl.co/readpost', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ message: "" })
+        });
+        const result = await response.json();
+        posts = result;
+        displayPosts();
+    } catch (error) {
+        console.error(error);
+    }
+};
+shoti() 
+function displayPosts() {
+    const postsList = document.getElementById("posts-list");
+    postsList.innerHTML = "";
+ 
+    try {
+        if (Array.isArray(posts)) {
+            for (let index = 0; index < posts.length; index++) {
+                const post = posts[index];
+                const postDiv = document.createElement("div");
+                postDiv.className = "post";
+                postDiv.innerHTML = `
+                <p><strong><img src="${post.image}" width="30" height="30">${post.anonymous}</strong></p>
+                <p>${post.text}</p>
+                <p>Likes: <span id="likes-${index}">${post.likes}</span></p>
+                <button onclick="likePost(${index})">Like</button>
+ 
+                <div>
+                    <input type="text" id="comment-${index}" placeholder="Leave a comment">
+                    <button onclick="addComment(${index})">Comment</button>
+                </div>
+ 
+                <div id="comments-${index}"></div>
+                `;
+                postsList.appendChild(postDiv);
+                //displayComments(index);
+            }
+        }
+    } catch (err) {
+        console.log(err);
+    }
+}
+ 
+async function addPost(text, anonymous) {
+    const currentDate = new Date();
+    const datetime = currentDate.toLocaleString();
+    const newPost = {
+        text,
+        anonymous,
+        datetime,
+        likes: 0,
+        comments: [],
+        image: "https://beebom.com/wp-content/uploads/2023/04/featured-new.jpg?w=730&h=487&crop=1&quality=75"
+    };
+    posts.push(newPost);
+    const response = await fetch('https://liby.wasieacuna.repl.co/addpost', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(newPost)
+    });
+    const result = await response.json();
+    displayPosts();
+    shoti() 
+}
+ 
+document.getElementById("post-button").addEventListener("click", () => {
+    const postText = document.getElementById("post-text").value;
+    const anonymousName = "Anonymous";
+    if (postText.trim() !== "") {
+        addPost(postText, anonymousName);
+        document.getElementById("post-text").value = "";
+    }
+});
+ 
+ 
+function likePost(index) {
+    posts[index].likes++;
+    displayPosts();
+}
+ 
+function addComment(postIndex) {
+    const commentText = document.getElementById(`comment-${postIndex}`).value;
+    if (commentText.trim() !== "") {
+        posts[postIndex].comments.push(commentText);
+        displayComments(postIndex);
+        document.getElementById(`comment-${postIndex}`).value = "";
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+/****let posts = [];
 
 
 
